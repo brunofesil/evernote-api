32e112fc0e70de70d04f756a2b2329fda5d97cd5
brunofesil <brfsilva@outlook.com>
2021-11-01 19:45:19 -0300

    Criada rota Show

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -7,14 +7,34 @@ router.post('/', withAuth, async (req, res) => {
   const { title, body } = req.body;
   
   try {
-    console.log(req.body)
+    /*console.log(req.body)*/
     let note = new Note({title: title, body: body, author: req.user._id});
     await note.save();
     res.status(200).json(note);
   } catch (error) {
     res.status(500).json({error: 'Problem to create new note.'});
-    /**console.log(error); */
+    /*console.log(error); */
   }
 })
+
+router.get('/:id', withAuth, async (req, res) => {
+  try {
+    const { id } = req.params;
+    let note = await Note.findById(id);
+    if(isOwner(req.user, note))
+      res.json(note)
+    else
+      res.status(403).json({error: 'Permission denied.'});
+  } catch (error) {
+    res.status(500).json({error: 'Problem to retrieve the note note.'});
+  }
+})
+
+const isOwner =(user, note) => {
+  if(JSON.stringify(user._id) == JSON.stringify(note.author._id))
+    return true;
+  else
+    return false;
+}
 
 module.exports = router;
